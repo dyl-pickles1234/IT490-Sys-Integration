85078f2f3b4a27de99ea0fdbbc0a023ee7780146
Dylan <dylankiddo10@gmail.com>
2025-10-18 15:18:12 -0400

    finished(?) post UI, works based on how many results pulled from db; made new post page UI that shows based on db pulls too eventually; placeholder pics

--- src/webserver/proj/forum.php
+++ src/webserver/proj/forum.php
@@ -3,16 +3,31 @@
 
 <body>
     <h1>Forum</h1>
-    <div class="forum_post_list_item">
-        <span class="post_list_title"><a href="post.php?post=<?php // post ID ?>">Title of Post</a></span>
-        <div style="display: inline; float: right; padding-top: 3;">
-            <span class="post_list_author">Author Name</span>
-            <span class="post_list_date">01/01/2000</span>
-            <span class="post_list_engagements">💚 0 | 💬 0</span>
-        </div>
-    </div>
-    <div class="forum_post_list_item">
-        wiggle2
-    </div>
 
+    <?php
+    // grab forum posts from db
+    // do in pages
+    $posts_on_page = [
+        [1, "Title of a Really Cool Post", "Author Name", "01/01/2000", 10, 3],
+        [3, "Title of a Second, Even Cooler Post", "Different Author", "01/01/2000", 0, 1]
+    ]; // ex
+    ?>
+
+    <?php for ($i = 0; $i < count($posts_on_page); $i++): ?>
+        <div class="forum_post_list_item">
+            <span class="post_list_title">
+                <a href="post.php?post=<?php echo $posts_on_page[$i][0] ?>">
+                    <?php echo $posts_on_page[$i][1] ?>
+                </a>
+            </span>
+            <div style="display: inline; float: right; padding-top: 3;">
+                <span class="post_list_author"><?php echo $posts_on_page[$i][2] ?></span>
+                <span class="post_list_date"><?php echo $posts_on_page[$i][3] ?></span>
+                <span class="post_list_engagements">
+                    💚 <?php echo $posts_on_page[$i][4] ?> |
+                    💬 <?php echo $posts_on_page[$i][5] ?>
+                </span>
+            </div>
+        </div>
+    <?php endfor; ?>
 </body>
