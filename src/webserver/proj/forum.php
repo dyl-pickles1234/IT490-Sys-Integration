46a549e3c54780f10bfd7b2f4c2b9fa116cb0b72
Dylan <dylankiddo10@gmail.com>
2025-10-18 00:43:49 -0400

    very start of forum list of posts

---
+++ src/webserver/proj/forum.php
@@ -0,0 +1,18 @@
+<?php include 'header.html'; ?>
+<html>
+
+<body>
+    <h1>Forum</h1>
+    <div class="forum_post_list_item">
+        <span class="post_list_title"><a href="post.php?post=<?php // post ID ?>">Title of Post</a></span>
+        <div style="display: inline; float: right; padding-top: 3;">
+            <span class="post_list_author">Author Name</span>
+            <span class="post_list_date">01/01/2000</span>
+            <span class="post_list_engagements">💚 0 | 💬 0</span>
+        </div>
+    </div>
+    <div class="forum_post_list_item">
+        wiggle2
+    </div>
+
+</body>
