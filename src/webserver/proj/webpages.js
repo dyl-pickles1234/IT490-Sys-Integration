2b1c6a781d95ff7c1102c73cf70ce4ae9e095ce4
Dylan <dylankiddo10@gmail.com>
2025-10-20 12:26:03 -0400

    converted to using generic request/handler system

--- src/webserver/proj/webpages.js
+++ src/webserver/proj/webpages.js
@@ -45,11 +45,6 @@ function getCookie(name) {
     return null;
 }
 
-function clearSessionCookie() {
-    document.cookie = "session_id=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    window.location.href = "/";
-}
-
 function HandleSessionCheckResponse(response) {
     var responseObj = JSON.parse(response);
     console.log(responseObj);
@@ -95,81 +90,20 @@ function getLoggedInUser() {
     return null;
 }
 
-function HandleLoginResponse(response) {
-    var responseObj = JSON.parse(response);
-    console.log(responseObj);
-    var res = responseObj[1];
-    var success;
-
-    if (res === false) {
-        success = false;
-    } else {
-        success = res[1];
-    }
-
-    //	document.getElementById("textResponse").innerHTML = response+"<p>";	
-    // document.getElementById("textResponse").innerHTML = "response: " + responseObj[0] + "<p>";
-    document.getElementById("textResponse").innerHTML = success ? "Login successful!" : "Login failed!";
-
-    if (success) {
-        // set cookie
-        document.cookie = "session_id=" + res[2] + "; path=/";
-        window.location.href = "home.php";
-    }
-}
-
-function SendLoginRequest(email, password) {
-    var request = new XMLHttpRequest();
-    request.open("POST", "webserver.php", true);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.onreadystatechange = function () {
-
-        if ((this.readyState == 4) && (this.status == 200)) {
-            HandleLoginResponse(this.responseText);
-        }
-    }
-    request.send("type=login&email=" + email + "&pword=" + password);
-}
-
 function GetLoginInfo() {
     email = document.getElementById("email_input").value;
     password = document.getElementById("password_input").value;
     // document.getElementById("textResponse").innerHTML = email + ' ' + password
-    SendLoginRequest(email, password);
-}
-
-function HandleRegisterResponse(response) {
-    var responseObj = JSON.parse(response);
-    console.log(responseObj);
-    var res = responseObj[1];
-    var success;
-
-    if (res === false) {
-        success = false;
-    } else {
-        success = res[1];
-    }
-
-    //	document.getElementById("textResponse").innerHTML = response+"<p>";	
-    // document.getElementById("textResponse").innerHTML = "response: " + responseObj[0] + "<p>";
-    document.getElementById("textResponse").innerHTML = success ? "Registration successful!" : "Registration failed!";
-
-    if (success) {
-        window.location.href = "login.php";
-    }
-}
-
-function SendRegisterRequest(email, password, f_name, l_name) {
-    var request = new XMLHttpRequest();
-    request.open("POST", "webserver.php", true);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.onreadystatechange = function () {
-
-        if ((this.readyState == 4) && (this.status == 200)) {
-            HandleRegisterResponse(this.responseText);
-        }
-    }
-    request.send("type=register&email=" + email + "&pword=" + password + "&f_name=" + f_name + "&l_name=" + l_name);
+    SendGenericRequest('login', { 'email': email, 'pword': password },
+        (res) => {
+            document.cookie = "session_id=" + res[2] + "; path=/";
+            window.location.href = "home.php";
+            document.getElementById("textResponse").innerHTML = "Login successful!";
+        },
+        (res) => {
+            document.getElementById("textResponse").innerHTML = "Login failed!";
+        });
+    // SendLoginRequest(email, password);
 }
 
 function GetRegistrationInfo() {
@@ -178,5 +112,13 @@ function GetRegistrationInfo() {
     f_name = document.getElementById("fname_input").value;
     l_name = document.getElementById("lname_input").value;
     // document.getElementById("textResponse").innerHTML = email + ' ' + password
-    SendRegisterRequest(email, password, f_name, l_name);
+    SendGenericRequest('register', { 'email': email, 'pword': password, 'f_name': f_name, 'l_name': l_name },
+        (res) => {
+            document.getElementById("textResponse").innerHTML = "Registration successful!";
+            window.location.href = "login.php";
+        },
+        (res) => {
+            document.getElementById("textResponse").innerHTML = "Registration failed!";
+        });
+    // SendRegisterRequest(email, password, f_name, l_name);
 }
